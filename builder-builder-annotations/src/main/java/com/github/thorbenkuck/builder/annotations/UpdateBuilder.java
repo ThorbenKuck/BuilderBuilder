65e802ee0f88f512a81cd5cf04c42ffa0b3362d0
thorben <thorben.kuck@gmail.com>
2019-08-06 20:55:25 +0200

    added todo annotation for update builder

---
+++ builder-builder-annotations/src/main/java/com/github/thorbenkuck/builder/annotations/UpdateBuilder.java
@@ -0,0 +1,14 @@
+package com.github.thorbenkuck.builder.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface UpdateBuilder {
+
+    // TODO Implement
+
+}
